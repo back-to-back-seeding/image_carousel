41798733addcb42ac59c7ff8252368d4a584a4ba
Chris Ciervo <christopherciervo@gmail.com>
2020-08-11 09:38:49 -0700

    Added css and style loaders to webpack

--- client/components/Images.jsx
+++ client/components/Images.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Images = () => (
-  <div>
+  <div className="test">
     <img className="photo" src="https://fec-image-carousel-photos.s3.us-east-2.amazonaws.com/image-23.jpg" alt="" />
   </div>
 );
