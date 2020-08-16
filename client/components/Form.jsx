0b88cd419535270c8cd87865d63cf36eea3fca66
Chris Ciervo <christopherciervo@gmail.com>
2020-08-16 11:57:41 -0700

    Added exitInput functionality to Form component

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -151,8 +151,8 @@ class Form extends React.Component {
 
   render() {
     return (
-      <NameThisList>
-        <Header>
+      <NameThisList onClick={this.state.inputTriggered ? this.exitInput : null}>
+        <Header onClick={this.exitInput}>
           <ExitButton onClick={this.props.hideForm}>
             <svg aria-hidden="true" role="presentation" focusable="false" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" style={{ height: 16, width: 16 }}>
               <path d="m6 6 20 20" />
