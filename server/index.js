5032094ab80c02e2631085a2936bf4b4fa462378
Chris Ciervo <christopherciervo@gmail.com>
2020-08-22 11:48:42 -0700

    Added dcokerfile and compose file to server, added node command for container to run when spun

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const model = require('../db/model.js');
+console.log('model hit!');
 
 const app = express();
 const PORT = 3004;
