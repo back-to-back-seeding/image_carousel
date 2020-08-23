0f3f46af18ed7fd3b4845f71bd28b9914d40f9fe
Chris Ciervo <christopherciervo@gmail.com>
2020-08-23 16:55:20 -0700

    Added server side compression for better performance

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
+const expressStaticGzip = require('express-static-gzip');
 const model = require('../db/model.js');
-console.log('model hit!');
 
 const app = express();
+app.use(compression());
+
 const PORT = 3004;
-app.use('/rooms/:room_id', express.static(path.join(__dirname, '../public')));
+// app.use('/rooms/:room_id', express.static(path.join(__dirname, '../public')));
+app.use('/rooms/:room_id', expressStaticGzip(path.join(__dirname, '../public')));
 
 app.get('/suggestedListings', (req, res) => {
   console.log('get req working!');
