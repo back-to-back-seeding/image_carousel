06544d74c3e5e2fcd7759146b93b0975329ad341
Shirley Wu <shirleyswwu@gmail.com>
2020-08-28 23:02:10 -0700

    this is example from pm-checkin

--- db-sdc/seeding-db.js
+++ db-sdc/seeding-db.js
@@ -1,3 +1,27 @@
 const fs = require('fs');
+const mongodb = require('index-mongoose.js');
 
-//json
+const reviews = [1, 10, 20, 40, 60];
+
+const seedData = (entries) => {
+  let dataStr = 'id reviews: ';
+  for (let i = 1; i < entries; i++) {
+    dataStr += `\n`;
+    dataStr += `${i}`;
+    dataStr += `${reviews[i%6]}`;
+    dataStr += `\n`;
+  }
+  return new Promise((resolve, reject) => {
+    fs.writeFile('mongo-db.txt', dataStr, (err) => {
+      if (err) {
+        reject (err);
+      } else {
+        resolve (dataStr);
+      }
+    });
+  });
+}
+
+seedData(10)
+  .then(() => {console.log('success')})
+  .catch(() => {console.log('oopsie')});
