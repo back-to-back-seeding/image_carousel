ebb850cdba51c3f78a82fdeeb5160f657c142a96
Shirley Wu <shirleyswwu@gmail.com>
2020-09-02 13:13:10 -0700

    fix syntax of schema

---
+++ db-sdc/index-postgress.sql
@@ -0,0 +1,47 @@
+-- postgreSQL database schema
+
+DROP DATABASE IF EXISTS moreplaces;
+CREATE DATABASE moreplaces;
+-- USE morePlaces;
+
+CREATE TABLE places (
+  id INT NOT NULL,
+  rating DECIMAL(3, 2) NOT NULL,
+  numOfReviews INT,
+  title VARCHAR(50) NOT NULL,
+  description VARCHAR(150),
+  rates INT,
+  superHost BOOL,
+  photo varchar(300),
+  PRIMARY KEY (id)
+  -- CHECK (rating>=0 AND rating <= 5),
+  -- CHECK (numOfReviews >=0),
+  -- CHECK (rates >=0)
+);
+
+CREATE TABLE relatedplaces (
+  id INT NOT NULL,
+  ref_placeId INT NOT NULL
+);
+--might not need
+CREATE TABLE users (
+  id INT NOT NULL,
+  folder VARCHAR(20) NOT NULL,
+  saved_placeId INT NOT NULL
+);
+-----------------------------------------------------------
+
+-- ALTER TABLE relatedPlaces ADD CONSTRAINT current_placeIds
+--   FOREIGN KEY (id)
+--   REFERENCES places (id);
+
+-- ALTER TABLE relatedPlaces ADD CONSTRAINT related_placeIds
+--   FOREIGN KEY (ref_placeId)
+--   REFERENCES places (id);
+
+-- ALTER TABLE users ADD CONSTRAINT saved_placeds
+--   FOREIGN KEY (saved_placeId)
+--   REFERENCES places (id);
+
+-- join all table
+-- select * from table users join table places where ...
